1ae7f086f1acf4a96e8ec8fdeff97cb94449a80e
yavorskijy <yavorskijy@gmail.com>
2026-03-10 21:36:36 +0000

    Add files via upload

---
+++ bot.py
@@ -0,0 +1,32 @@
+from telegram import Update
+from telegram.ext import ApplicationBuilder, MessageHandler, filters, ContextTypes
+import os
+
+TOKEN = os.getenv("8757949179:AAHZ4HiqD1xkjPp_0RVo9_GD00DZhsUh5m8")
+
+allowed_words = ["wts", "wtb", "#wts", "#wtb"]
+
+async def filter_messages(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    message = update.message
+    
+    if not message:
+        return
+        
+    if not message.text:
+        return
+
+    text = message.text.lower()
+
+    if not any(word in text for word in allowed_words):
+        try:
+            await message.delete()
+        except:
+            pass
+
+app = ApplicationBuilder().token(TOKEN).build()
+
+app.add_handler(MessageHandler(filters.TEXT, filter_messages))
+
+print("Bot started")
+
+app.run_polling()
